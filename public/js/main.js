01ba333a0c13fa82efbc3a2d0ff3e191cd8e9c98
gsilve02 <genevieve.silverman@tufts.edu>
2017-04-22 12:10:29 -0400

    Added Tina counter

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,9 @@
-
+var clicks = 0;
 function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
-
-  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
+  clicks++;
+  $("#demo").html(clicks + " tinas");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#gif").append(`<img class="img-circle" src="/images/tina_dance.gif" />`);
